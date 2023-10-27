b8e56c9eeb77456ad9ff2d7e28d38af1e561ec08
NODA Masaki <noda@beatcraft.com>
2023-10-27 08:43:19 +0000

    max_requests

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,6 +4,8 @@ import os
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2
 worker_class = "uvicorn.workers.UvicornWorker"
+max_requests = 500
+max_requests_jitter = 200
 
 pidfile = "/opt/iotuploader/run/iotuploader.pid"
 accesslog = "/opt/iotuploader/log/access.log"

13f6b902ec55fc2022e86847f092aedcdf3cdd45
NODA Masaki <noda@beatcraft.com>
2023-11-02 07:10:20 +0000

    test session

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,43 @@ import sys
 sys.path.append("/opt/iotuploader/src/iot-uploader")
 
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker, scoped_session
 
-from iotuploader.database import SessionLocal
+from iotuploader.main import app
+from iotuploader.database import get_db
+from iotuploader.config import get_settings
 
-@pytest.fixture()
+class TestSession(Session):
+    def commit(self):
+        self.flush()
+        self.expire_all()
+
+settings = get_settings()
+
+
+@pytest.fixture(scope="function")
 def db():
-    with SessionLocal() as session:
+    engine = create_engine(settings.db_url, pool_pre_ping=True)
+    TestSessionLocal = scoped_session(
+        sessionmaker(
+            class_=TestSession,
+            autocommit=False,
+            autoflush=False,
+            bind=engine
+        )
+    )
+
+    with TestSessionLocal() as session:
+        def get_test_db():
+            try:
+                yield session
+            except:
+                session.rollback()
+
+        app.dependency_overrides[get_db] = get_test_db
+
         yield session
+
+        session.rollback()
 

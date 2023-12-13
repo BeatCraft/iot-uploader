198e6b63083cf46bbd18aeb05815f858189be73a
NODA Masaki <noda@beatcraft.com>
2023-12-13 14:05:19 +0900

    timeout settings

--- setup/uploader.conf.py
+++ setup/uploader.conf.py
@@ -6,6 +6,7 @@ workers = multiprocessing.cpu_count() * 2
 worker_class = "uvicorn.workers.UvicornWorker"
 max_requests = 500
 max_requests_jitter = 200
+timeout = 1200
 
 pidfile = "/opt/iotuploader/run/uploader.pid"
 accesslog = "/opt/iotuploader/log/access.log"
